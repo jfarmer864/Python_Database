24ba0769675e889313c5d9a7bb268472a37cf51a
jfarmer864 <57761247+jfarmer864@users.noreply.github.com>
2019-11-29 10:20:51 +0000

    Add files via upload

--- Homework.py
+++ Homework.py
@@ -30,23 +30,22 @@ class Connection:
         return(cursor)
 
 class ReadOperation:
-    def __init__(self, connection_object, table, select_string):
+    def __init__(self, table, select_string):
         self.select_string = select_string
         self.table = table
-        self.connection_object = connection_object
 
-    def do_it(self):
+    def do_it(self, connector):
         sql_select = "SELECT {} FROM {}".format(self.select_string, self.table)
-        self.connection_object.execute(sql_select)
-        for row in self.connection_object:
+        connector.execute(sql_select)
+        for row in connector:
             print(row)
 
-class AddOperation:
+class InsertOperation:
     def __init__(self, record):
         for key, value in record.__dict__.items():
             setattr(self, key, value)
 
-    def do_it(self):
+    def do_it(self, connector):
         column_string = ""
         columns = iter(self.__dict__.keys())
         next(columns, None)
@@ -62,35 +61,63 @@ class AddOperation:
         if value_string.endswith(','):
             value_string = value_string[:-1]
         sql_add = "INSERT INTO " + self.table_name + "(" + column_string + ") VALUES(" + value_string + ")"
-        # print(sql_add)
-        connect = Connection('localhost,1433', 'Northwind', 'sa', 'Passw0rd2018').make_connection()
-        connect.execute(sql_add)
-        connect.commit()
+        connector.execute(sql_add)
+        connector.commit()
 
+
+class DeleteOperation():
+    def __init__(self, table_name, condition):
+        self.table_name = table_name
+        self.condition = condition
+
+    def do_it(self, connector):
+        sql_delete = "DELETE FROM {} WHERE {}".format(self.table_name, self.condition)
+        connector.execute(sql_delete)
+        connector.commit()
+
+
+class UpdateOperation():
+    def __init__(self, table_name, column_update, condition):
+        self.table_name = table_name
+        self.column_update = column_update
+        self.condition = condition
+
+    def do_it(self, connector):
+        sql_update = "UPDATE {} SET {} WHERE {}".format(self.table_name, self.column_update, self.condition)
+        connector.execute(sql_update)
+        connector.commit()
 
 
 class ProgramMain:
     def __init__(self):
-        self.result = ""
+        self.connector = ""
 
-    def run(self):
-        #run program
-        return
+    def connection(self, server, database_name, user_name, password):
+        connector = Connection(server,database_name,user_name,password).make_connection()
+        self.connector = connector
 
-    def crud_operation(self):
-        # I need an object representing the table
-        # I need a SQL statement to run
-        # I need the connection object to send query to the database
-        result = ""
+    def insert(self, record_object):
+        insert_object = InsertOperation(record_object)
+        insert_object.do_it(self.connector)
 
-# main_prog = ProgramMain()
-# main_prog.run()
+    def read(self, table_name, select_string):
+        read_object = ReadOperation(table_name, select_string)
+        read_object.do_it(self.connector)
 
-# connect = Connection('localhost,1433','Northwind','sa','Passw0rd2018').make_connection()
-# print(connect)
+    def delete(self, table_name, condition):
+        delete_object = DeleteOperation(table_name, condition)
+        delete_object.do_it(self.connector)
 
-region = Region("'5'","'centre'")
-ship = Shippers('12','Big Ships','999')
-test1 = AddOperation(region)
-test2 = AddOperation(ship)
-test1.do_it()
+    def update(self, table_name, column_name, condition):
+        update_object = UpdateOperation(table_name, column_name, condition)
+        update_object.do_it(self.connector)
+
+
+# region = Region("'6'","'Outback'")
+# ship = Shippers('12','Big Ships','999')
+# mainprogram = ProgramMain()
+# mainprogram.connection('localhost,1433','Northwind','sa','Passw0rd2018')
+# mainprogram.read("Region","*")
+# mainprogram.update("Region","RegionDescription = 'Far away","RegionID = 4")
+# mainprogram.insert(region)
+# mainprogram.delete("Region","RegionID = 4")
